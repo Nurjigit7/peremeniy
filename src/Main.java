f25c91c7d0d38ec887cf73d273289596809477bf
626 <nurjigitumarov22@gmail.com>
2023-07-10 19:06:45 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,127 @@
+import java.io.FilterOutputStream;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+import static jdk.internal.org.jline.utils.Colors.s;
+
+public class Main {
+    public static void main(String[] args) {
+      //  System.out.println("hello java");//жаны сапка чыгарат
+      //  System.out.print("hello world ");//бир сапка чыгарат
+       // %d bul butun sandar uchun
+       // %f bul bolchok sandar uchun
+       // %s bul sozdor uchun
+       // %c bul sumboldor uchun
+     //   System.out.printf("%d , %f , %s %d %c",50,25.5,"java Moscow ",7,'e');
+     //   System.out.printf("%d+%d=%d",50,50,50+50);
+//        byte b = -128,127;
+//        short sh = -32768,32767;
+//        int i = -243422245,243422244;
+//        long l = 3243434345555553535l;
+//
+//        //bolchok sandar uchun
+//        float f = 442.4432f;
+//        double d = 4343.5344;
+//        double d1 = 4343.5344;
+//
+//        boolean b1 = true;
+//        boolean b2 = false;
+//
+//        char ch = '@';
+//        //  System.out.println(ch);
+
+//         int san =45;
+//        int san1 =450;
+//         System.out.println(san+san1);
+        // System.out.printf("%d",san1);
+      /*  Scanner scan=new Scanner(System.in);
+        System.out.println("birinchi san");
+        int firstNumber=scan.nextInt();
+        System.out.println("ekinchi san");
+        int secondNumber=scan.nextInt();
+        System.out.println("joop");
+        System.out.println(firstNumber+secondNumber);*/
+
+
+//    Scanner scanner=new Scanner(System.in);
+//        int a=scanner.nextInt();
+//        int r=scanner.nextInt();
+//        int p=scanner.nextInt();
+//        int summa=a+r+p;
+//        System.out.println(summa);
+//        int ncb=a*r*p;
+//        System.out.println(ncb);
+//        double arifmetic=summa;
+//        System.out.println(arifmetic/3);
+
+
+     /*   Scanner scanner=new Scanner(System.in);
+        int a=scanner.nextInt();
+        int j=scanner.nextInt();
+        int summa=a*j;
+        System.out.println(summa);
+        int umma=a/j;
+        System.out.println(umma);
+        int mma=a+j;
+        System.out.println(mma);
+        int ma=a-j;
+        System.out.println(ma);*/
+
+      /*  Scanner scanner=new Scanner(System.in);
+        int a=scanner.nextInt();
+        int s=scanner.nextInt();
+        int umma=a-s;
+        System.out.println(umma);*/
+
+     /*  Scanner scanner=new Scanner(System.in);
+        double o=scanner.nextDouble();
+        double mma=o-0.2;
+        System.out.println(mma);*/
+
+      /*  Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        System.out.println("bul menin jashym "+n);
+
+        int m=scanner.nextInt();
+        System.out.println("bul menin jashym emes"+m);*/
+       /*  int a=0;
+        int b=1;
+        int v=2;
+        int n=3;
+        int m=4;
+        int s=5;
+        int r=6;
+        int y=7;
+        int u=8;
+        int i=9;
+        double summa=(a+b+v+n+m+s+r+y+u+i);
+        double arifmetcs=summa /10;
+        System.out.println(arifmetcs);*/
+
+       /*  Scanner scanner=new Scanner(System.in);
+        int a=scanner.nextInt();
+        int r=scanner.nextInt();
+        int s=scanner.nextInt();
+        int summa=a+r+s;
+        System.out.printf( "%d+%d+%d=%d",a,r,s,a+r+s);
+        int um=a*r*s;
+        System.out.printf("%d*%d*%d=%d",a,r,s,a*r*s);
+
+        double o= (double) summa/3;
+        System.out.println(o);*/
+
+//       byte b=60;
+//       short f=75;
+//       int s=b+f;
+//        System.out.println(s);
+//       Scanner scanner = new Scanner(System.in);
+//        System.out.println("birinchi san ");
+//        int san = scanner.nextInt();
+//        System.out.println("ekinchi san ");
+//        int san2 = scanner.nextInt();
+//        System.out.println("Joop");
+//        System.out.println(san+san2);
+
+
+    }
+}
